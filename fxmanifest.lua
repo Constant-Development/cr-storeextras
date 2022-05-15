9d62909a8b22d3ead55054ea517631829255dcac
Fast-Lane RolePlay <timothyphilipp14@gmail.com>
2022-05-15 01:40:30 -0400

    Easy Client Understand

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,13 @@ description 'Local Store Extra Options/Functions'
 version '2.0'
 
 shared_script 'config.lua'
-client_script 'client/client.lua'
+client_scripts {
+    'client/main.lua',
+    'client/slushies.lua',
+    'client/sluckybuckydonut.lua',
+    'client/icecream.lua',
+    'client/gumballs.lua',
+}
 server_script 'server/server.lua'
 
 lua54 'yes'
